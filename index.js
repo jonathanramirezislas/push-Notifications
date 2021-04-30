131ff2f6fddd28596bfb5eadf5ac3d81262dca19
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-04-30 13:09:22 -0500

    :sparkles: client

--- index.js
+++ index.js
@@ -3,28 +3,38 @@ const webpush = require('web-push');
 const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname,"client")));
+
 app.use(express.json());
 
 //put en env later XD 
 const publicVapidKey = 'BIa9BJCII7IvRvqsPXGrLStWLCsUlNZ_RE9sKj8k3grfLUYpsNkMRUOKWzJKQjy5s9tTJhML1zGjmYf8FcJ0_W8';
 const privateVapidKey = 'ysJVT6aw6eNFZgmS5hRB-J1FB2wpjsbixlL3aFHT0ak';
 
-webpush.setVapidDetails('mailo: jona@gmail.com',publicVapidKey, privateVapidKey);
-
-//Suscribe Route 
-app.post('/suscribe', (req, res) => {
-    //Get pushSubscription object
+webpush.setVapidDetails(
+    "mailto:jona@tjona.com",
+    publicVapidKey,
+    privateVapidKey
+  );
+  
+  // Subscribe Route
+  app.post("/subscribe", (req, res) => {
+    // Get pushSubscription object
     const subscription = req.body;
-
-    //resource created
+  
+    // Send 201 - resource created
     res.status(201).json({});
-
-    //create payload
-    const payload = JSON.stringify({title: 'Push Test'})
-
-    //Pass object into sendNotification
-    webpush.sendNotification(subscription, payload).catch(err => console.log(err));
-});
-
-const port = 5000;
-app.listen(port, ()=> console.log(`Server started on port ${port}`))
+  
+    // Create payload
+    const payload = JSON.stringify({ title: "Push Test" });
+  
+    // Pass object into sendNotification
+    webpush
+      .sendNotification(subscription, payload)
+      .catch(err => console.error(err));
+  });
+  
+  const port = 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`));
